918f9643f044e4017670d48d417a3184c616e8dc
banikhurana04 <banikhurana04@gmail.com>
2025-06-17 15:50:41 +0530

    Add .gitignore and remove personal files from tracking

--- flask-two-factor-auth/create_db.py
+++
@@ -1,9 +0,0 @@
-from src import create_app  # import the factory
-from src.db import db
-from src.accounts.models import User, BehaviorProfile, LoginHistory
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-    print("Database tables created successfully!")
